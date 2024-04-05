14ce8672a3c25467c04dc23b62d1c380725e6ae1
Jana Hayaly <janahayaly@Janas-MacBook-Air-2.local>
2024-04-04 23:58:28 -0400

    ok now

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -5,23 +5,6 @@ import Login from './login';
 import SignUp from './signup';
 import { useNavigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
-//import * as express from 'express';
-//import * as cors from 'cors';
-
-//const express = require('express')
-//var cors = require('cors')
-/*const app = express()
- 
-app.use(cors())
- 
-app.get('/products/:id', (req, res) => {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
- 
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log('CORS-enabled web server listening on port 80')
-})*/
 
 const App = () => {
   const navigate = useNavigate();
